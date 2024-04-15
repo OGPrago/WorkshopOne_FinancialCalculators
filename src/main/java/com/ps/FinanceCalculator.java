3eb96f814a5c19b280ff42a75161586a3d623a7c
Jeremy <ogprago@gmail.com>
2024-04-14 20:44:47 -0400

    Added Present Value Calculator

--- src/main/java/com/ps/FinanceCalculator.java
+++ src/main/java/com/ps/FinanceCalculator.java
@@ -7,7 +7,7 @@ public class FinanceCalculator {
         Scanner scanner = new Scanner(System.in);
 
         //Asking user which calculator they want to use
-        System.out.println("Which calculator would you like to use? (1)Mortgage, (2)Future Value");
+        System.out.println("Which calculator would you like to use? (1)Mortgage, (2)Future Value, (3)Present Value");
         byte userIn = scanner.nextByte();
 
         //Creating if statements for each calculator
@@ -35,6 +35,17 @@ public class FinanceCalculator {
                     + " years, your CD's ending balance will be $" + endingBal + " and you would have earned $" + balanceDifference + "in interest");
 
         }
+        else if(userIn ==3) {
+            System.out.println("Enter the monthly payout: ");
+            float monthlyPayout = scanner.nextFloat();
+            System.out.println("Enter the expected interest rate: ");
+            float intRate = scanner.nextFloat();
+            System.out.println("Enter number of years to payout: ");
+            int years = scanner.nextInt();
+            float annuity = presentValueCalc(monthlyPayout, intRate, years);
+            System.out.println("To fund an annuity that pays $" + monthlyPayout + " monthly for " + years + " years and earns and expected " + intRate + "% interest, "
+            + "you would need to invest $" + annuity + " today.");
+        }
     }
     //Creating mortgage calculator, Calculating monthly mortgage
     public static float mortgageCalc(float loan, float rate, int term) {
@@ -43,10 +54,18 @@ public class FinanceCalculator {
         return mortgage;
     }
 
-    //Creating CD Calculator
+    //Creating CD Calculator, calculating future value
     public static float cdCalc(float deposit, float rate, int years) {
         float rateDecimal = rate / 100;
         float endingBal = (float) (deposit * Math.pow(1 + rateDecimal / 365, 365 * years));
         return endingBal;
+    }
+    //Creating present value calculator, calculate present value of ordinary annuity
+    public static float presentValueCalc(float monthlyPay, float intRate, int years) {
+        float decimalIntRate = intRate / 100;
+        float periodIntRate = decimalIntRate / 12;
+        int totalNumPeriods = years * 12;
+        float presentValue = (float) (monthlyPay * ((1 - (Math.pow(1 + periodIntRate, - totalNumPeriods))) / periodIntRate));
+        return presentValue;
     }
 }
